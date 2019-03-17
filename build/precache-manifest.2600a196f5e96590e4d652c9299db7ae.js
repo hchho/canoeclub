cf80df3618ff79267f84c7877cb69b26c2cebaa5
Henry Ho <henrychho89@gmail.com>
2019-03-17 00:31:43 -0700

    Added production build

---
+++ build/precache-manifest.2600a196f5e96590e4d652c9299db7ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f282f1a7d618410822c",
+    "url": "/canoeclub/static/js/runtime~main.2d15526e.js"
+  },
+  {
+    "revision": "1c5686cc6f741ca1bf1e",
+    "url": "/canoeclub/static/js/main.c45268a9.chunk.js"
+  },
+  {
+    "revision": "b8417ed9efb2134fafcd",
+    "url": "/canoeclub/static/js/2.c35388e8.chunk.js"
+  },
+  {
+    "revision": "1c5686cc6f741ca1bf1e",
+    "url": "/canoeclub/static/css/main.dd3136e2.chunk.css"
+  },
+  {
+    "revision": "c3a0227acdf09188c117535c6d1b16b5",
+    "url": "/canoeclub/index.html"
+  }
+];
